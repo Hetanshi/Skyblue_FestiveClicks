356a0e3126480094c8321ae237a73865fc4ce6d5
Hetanshi Patel <hetanshipatel@Ringer1208.local>
2025-09-11 01:09:42 +0530

    build changes

--- app.py
+++ app.py
@@ -48,5 +48,6 @@ def frame():
     return send_from_directory("static", "navratri_frame.png")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port, debug=False)
+
